fb7604edf644103893f607dab7ae0ad0ca0cce7b
Pablo <osoriosalazarpablo@gmail.com>
2023-02-19 00:57:04 +0300

    Tasks 1 and 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,22 +10,28 @@ public_users.post("/register", (req,res) => {
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
-// Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.send(JSON.stringify({books},null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    const keys = Object.keys(books);
+    const respo = [];
+    for (let index = 0; index < keys.length; index++) {
+      const booki = books[keys[index]];
+      if(booki['author'] == author) {
+          respo.push(booki)
+      }
+    }
+    res.send(respo);
 });
 
 // Get all books based on title
