0fda40ef7135c30a2f11e5378ebd111a2ef1119d
Pablo <osoriosalazarpablo@gmail.com>
2023-02-20 14:07:24 +0300

    Finished async tasks

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,38 +20,50 @@ public_users.post("/register", (req,res) => {
 });
 
 public_users.get('/',function (req, res) {
-  res.send(JSON.stringify({books},null,4));
+  const get_books = new Promise((resolve, reject) => {
+    resolve(res.send(JSON.stringify({books}, null, 4)));
+  });
+  get_books.then(() => console.log("Promise for Task 10 resolved"));
 });
 
 public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
-  res.send(books[isbn]);
- });
+  const get_books_by_isbn = new Promise((resolve, reject) => {
+    const isbn = req.params.isbn;
+    resolve(res.send(books[isbn]));
+  });
+  get_books_by_isbn.then(() => console.log("Promise for Task 11 resolved"));
+});
   
 public_users.get('/author/:author',function (req, res) {
-  const author = req.params.author;
-  const keys = Object.keys(books);
-  const respo = [];
-  for (let index = 0; index < keys.length; index++) {
-    const booki = books[keys[index]];
-    if(booki['author'] == author) {
-        respo.push(booki)
+  const get_books_by_author = new Promise((resolve, reject) => {
+    const author = req.params.author;
+    const keys = Object.keys(books);
+    const respo = [];
+    for (let index = 0; index < keys.length; index++) {
+      const booki = books[keys[index]];
+      if(booki['author'] == author) {
+          respo.push(booki)
+      }
     }
-  }
-  res.send(respo);
+    resolve(res.send(respo));
+  });
+  get_books_by_author.then(() => console.log("Promise for Task 12 resolved"));
 });
 
 public_users.get('/title/:title',function (req, res) {
-  const title = req.params.title;
-  const keys = Object.keys(books);
-  const respo = [];
-  for (let index = 0; index < keys.length; index++) {
-    const booki = books[keys[index]];
-    if(booki['title'] == title) {
-        respo.push(booki)
+  const get_books_by_title = new Promise((resolve, reject) => {
+    const title = req.params.title;
+    const keys = Object.keys(books);
+    const respo = [];
+    for (let index = 0; index < keys.length; index++) {
+      const booki = books[keys[index]];
+      if(booki['title'] == title) {
+          respo.push(booki)
+      }
     }
-  }
-  res.send(respo);
+    resolve(res.send(respo));
+  });
+  get_books_by_title.then(() => console.log("Promise for Task 13 resolved"));
 });
 
 public_users.get('/review/:isbn',function (req, res) {
